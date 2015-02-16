099f2a2cd231076156bfa4e7fe46f7f69913ff4a
nikhil <nkmittal4994@gmail.com>
2015-02-17 01:48:51 +0530

    user and artist schemas

--- tagger.py
+++ tagger.py
@@ -1,5 +1,6 @@
 from mutagen.mp3 import MP3
 import os
+import sqlite3 as sql
 """
 audio = MP3("/mnt/E4481D43481D1640/Moosik/Bollywood/Sham.mp3")
 songtitle = audio["TIT2"]
@@ -10,14 +11,21 @@ for x in audio:
 """
 ctr=0
 types=[]
-for root, dirs, files in os.walk("/mnt/E4481D43481D1640/Moosik"):
-	for x in files:
-		name,_,typ=x.partition('.')
-		if typ not in ['jpg','png','jpeg','txt','ini','db','tif']:
-			#process file Todo: insert attributes of files in db
-			print(x)
-			if typ not in types:
-				types.append(typ)
-			ctr=ctr+1
-print(ctr)
-print(types)
+con = sql.connect('lfm.db')
+with con:
+	cur = con.cursor()
+	for root, dirs, files in os.walk("/mnt/E4481D43481D1640/Moosik/MAGIC!"):
+		for x in files:
+			name,_,typ=x.partition('.')
+			data=()
+			if typ not in ['jpg','png','jpeg','txt','ini','db','tif']:
+				#process file Todo: insert attributes of files in db
+				audio = MP3(os.path.join(root,x))
+				#data=data+(audio["TIT2"],audio["TPE1"],)
+				songtitle = audio["TIT2"].text[0]
+				artist = audio["TPE1"].text[0]
+				album = audio["TALB"].text[0]
+				cur.execute("INSERT INTO Songs(Name,Artist) VALUES(?,?);",(songtitle,artist))
+				cur.execute("INSERT INTO Artist(Name) VALUES(?);",(artist,))
+				if typ not in types:
+					types.append(typ)
